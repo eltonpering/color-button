64a63f88ac320da3b4080dc417dfcbd3335d9eeb
Elton A Pering <elton.pering@ambevtech.com.br>
2022-07-16 19:42:43 -0300

    Add label to button

--- src/App.test.js
+++ src/App.test.js
@@ -21,5 +21,32 @@ test('button has correct initial color', () => {
 });
 
 
+test('initial conditions', () => {
+  render(<App />);
 
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
+test('Checkbox disables button on first click and enables on second click', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
 
