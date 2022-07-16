4287b885e5b378cc1b0878de1ef33654a2e84ced
Elton A Pering <elton.pering@ambevtech.com.br>
2022-07-16 13:18:24 -0300

    test - change color button red to blue

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -8,7 +8,18 @@ test('button has correct initial color', () => {
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   // expect the background color to be red
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  //click the button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
+
+
 
 
